cd1962e16b6652c3fe1e60215150f8c44e8b9fd9
MorshedAl <123994547+MorshedAl@users.noreply.github.com>
2023-06-03 11:22:11 +0600

    Add files via upload

---
+++ float,double,typecasting.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // typecasting
+    double z=5.28490134;
+    int y=(int)z;
+    cout<<y<<endl;
+
+    // print float/double
+    float a=7.02585;
+    cout<<a<<endl;
+    cout<<fixed<<setprecision(2)<<a<<endl;
+
+
+
+    double b=15.893027999127;
+    cout<<b<<endl;
+    cout<<fixed<<setprecision(4)<<b<<endl;
+
+
+
+    return 0;
+}
